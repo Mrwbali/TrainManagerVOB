0c1bf83cc2ab132e2974981db1c71239b5e96c03
Mrwbali <152252@vutbr.cz>
2015-11-20 23:46:04 +0100

    Added max speed

--- TrainMAnager/TrainMAnager/TrainMAnager.cpp
+++ TrainMAnager/TrainMAnager/TrainMAnager.cpp
@@ -12,8 +12,10 @@
 int _tmain(int argc, _TCHAR* argv[])
 {	
 	std::string command_string;
+
 	std::vector<std::shared_ptr< Wagon >> train_cars;
-	
+
+	int max_train_speed_i = 10000;
 	train_cars.push_back(std::shared_ptr<Wagon>(new MotorLocomotive("T211", 50000, 40, 118, 147, MotorLocomotive::ELECTRIC)));
 
 	train_cars.push_back(std::shared_ptr<Wagon>(new Cistern("Zacs", 22700, 40, 83000, false)));
@@ -26,7 +28,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		std::cout << train_cars.size() << std::endl;
 		train_cars.at(i)->getStatus();
 		std::cout << "--------------------------" << std::endl;
+		if (train_cars.at(i)->getMaxSpeed() < max_train_speed_i)
+		{
+			max_train_speed_i = train_cars.at(i)->getMaxSpeed();
+		}
 	}
+	std::cout << "Maximalni rychlost soustavy je: " << max_train_speed_i << " km/h" << std::endl;
 	std::cin >> command_string;
 	return 0;
 }
